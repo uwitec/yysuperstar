a285446fe411d15278acdbdae9be8b9ff396aa88
yrenjie289@gmail.com <yrenjie289@gmail.com@0c541076-087d-6d29-ee78-a00999c07128>
2013-04-17 16:48:59 +0000

    1.新增ShoppingCar UI布局

--- trunk/gen/com/kfcreservation/R.java
+++ trunk/gen/com/kfcreservation/R.java
@@ -22,24 +22,34 @@ public final class R {
     }
     public static final class drawable {
         public static final int bt=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int loginbg=0x7f020002;
-        public static final int nextbutton=0x7f020003;
+        public static final int clearbutton=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int loginbg=0x7f020003;
+        public static final int nextbutton=0x7f020004;
+        public static final int nextbuttonsmall=0x7f020005;
+        public static final int shoppingcarb=0x7f020006;
     }
     public static final class id {
-        public static final int MenuPage=0x7f080007;
-        public static final int MyAddsPage=0x7f080009;
-        public static final int RelativeLayout1=0x7f080004;
-        public static final int ShopingCarPage=0x7f080008;
-        public static final int action_settings=0x7f08000c;
+        public static final int MenuPage=0x7f08000e;
+        public static final int MyAddsPage=0x7f080010;
+        public static final int RelativeLayout1=0x7f08000b;
+        public static final int ShopingCarPage=0x7f08000f;
+        public static final int action_settings=0x7f080013;
         public static final int bt_next=0x7f080002;
         public static final int button1=0x7f080003;
         public static final int cb_rm=0x7f080001;
         public static final int et_phonenum=0x7f080000;
-        public static final int iv_image=0x7f080006;
-        public static final int tabhost=0x7f08000a;
-        public static final int tv_text=0x7f080005;
-        public static final int viewpager=0x7f08000b;
+        public static final int imageButton1=0x7f080009;
+        public static final int imageView1=0x7f08000a;
+        public static final int iv_image=0x7f08000d;
+        public static final int listView1=0x7f080005;
+        public static final int scrollView1=0x7f080004;
+        public static final int tabhost=0x7f080011;
+        public static final int textView1=0x7f080006;
+        public static final int textView2=0x7f080007;
+        public static final int textView3=0x7f080008;
+        public static final int tv_text=0x7f08000c;
+        public static final int viewpager=0x7f080012;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
