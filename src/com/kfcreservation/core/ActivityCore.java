476420c9e194121a44cf4268b6975da61f48e324
yrenjie289 <yrenjie289@gmail.com>
2013-04-22 13:08:26 +0000

    1.提交一个临时版，拼合彬哥的新界面 2.实现ListView中的按钮点击事件

--- src/com/kfcreservation/core/ActivityCore.java
+++ src/com/kfcreservation/core/ActivityCore.java
@@ -9,23 +9,65 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Environment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.SimpleAdapter;
 import android.widget.SimpleAdapter.ViewBinder;
 
 import com.kfcreservation.R;
+import com.kfcreservation.control.KFCMenu.ViewHolder;
 import com.kfcreservation.dao.impl.FoodAllDaoImpl;
 
 public class ActivityCore {
+	
+//	public SimpleAdapter getFoodImageAdapter(Context context, int FoodTypeId) {
+//		
+//		String[] f = { "name", "price", "imgs" };
+//		int[] t = { R.id.tv_name, R.id.tv_price, R.id.im_img };
+//		
+//		FoodAllDaoImpl fad = new FoodAllDaoImpl();
+//		List<HashMap<String, Object>> imageList = fad.getFoodAllType(context, FoodTypeId);
+//
+//		List<HashMap<String, Object>> ls = new ArrayList<HashMap<String, Object>>();
+//
+//		for (HashMap<String, Object> k : imageList) {
+//
+//			HashMap<String, Object> mp = null;
+//			mp = new HashMap<String, Object>();
+//			mp.put("name", k.get("Name"));
+//			mp.put("price", k.get("Price"));
+//			mp.put("imgs", getBitmap(k.get("Img").toString()));
+//			ls.add(mp);
+//		}
+//
+//		SimpleAdapter adapter = new SimpleAdapter(context, ls,
+//				R.layout.menu_list, f, t);
+//		adapter.setViewBinder(new ViewBinder() {
+//
+//			@Override
+//			public boolean setViewValue(View view, Object data,
+//					String textRepresentation) {
+//				if (view instanceof ImageView && data instanceof Bitmap) {
+//					ImageView iv = (ImageView) view;
+//					iv.setImageBitmap((Bitmap) data);
+//					return true;
+//				}
+//				return false;
+//			}
+//		});
+//		
+//		return adapter;
+//	}
+	
+	public MyAdapter getFoodImageMyAdapter(Context context, int FoodTypeId, ViewHolder holder) {
 
-	public SimpleAdapter getFoodImageAdapter(Context context, int FoodTypeId) {
-		String[] f = { "name", "price", "imgs" };
-		int[] t = { R.id.tv_name, R.id.tv_price, R.id.im_img };
+		ViewHolder holderL = holder;
+		
 		FoodAllDaoImpl fad = new FoodAllDaoImpl();
-		// List<KFC> kfcs = fb.getbytype(this, idr);
-		List<HashMap<String, Object>> imageList = fad.getFoodAllType(context,
-				FoodTypeId);
+		List<HashMap<String, Object>> imageList = fad.getFoodAllType(context, FoodTypeId);
 
 		List<HashMap<String, Object>> ls = new ArrayList<HashMap<String, Object>>();
 
@@ -39,25 +81,27 @@ public class ActivityCore {
 			ls.add(mp);
 		}
 
-		SimpleAdapter adapter = new SimpleAdapter(context, ls,
-				R.layout.menu_list, f, t);
-		adapter.setViewBinder(new ViewBinder() {
-
-			@Override
-			public boolean setViewValue(View view, Object data,
-					String textRepresentation) {
-				if (view instanceof ImageView && data instanceof Bitmap) {
-					ImageView iv = (ImageView) view;
-					iv.setImageBitmap((Bitmap) data);
-					return true;
-				}
-				return false;
-			}
-		});
-
+		MyAdapter adapter = new MyAdapter(context, ls, holderL);
+		
+//		SimpleAdapter adapter = new SimpleAdapter(context, ls,
+//				R.layout.menu_list, f, t);
+//		adapter.setViewBinder(new ViewBinder() {
+//
+//			@Override
+//			public boolean setViewValue(View view, Object data,
+//					String textRepresentation) {
+//				if (view instanceof ImageView && data instanceof Bitmap) {
+//					ImageView iv = (ImageView) view;
+//					iv.setImageBitmap((Bitmap) data);
+//					return true;
+//				}
+//				return false;
+//			}
+//		});
+		
 		return adapter;
 	}
-
+	
 	public Bitmap getBitmap(String imageName) {
 		String path = Environment.getExternalStorageDirectory()
 				+ File.separator + "kfcpng" + File.separator + imageName
