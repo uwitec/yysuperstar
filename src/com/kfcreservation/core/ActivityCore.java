61539fd701d9cf9b83b96cf55bcb72b03c0f7957
yrenjie289 <yrenjie289@gmail.com>
2013-04-22 06:01:41 +0000

    1.代码整理 2.优化KFCMenu中mLvType.setOnItemClickListener的switch结构

--- src/com/kfcreservation/core/ActivityCore.java
+++ src/com/kfcreservation/core/ActivityCore.java
@@ -1,6 +1,68 @@
 package com.kfcreservation.core;
 
-import android.app.Activity;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-public class ActivityCore extends Activity {
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SimpleAdapter;
+import android.widget.SimpleAdapter.ViewBinder;
+
+import com.kfcreservation.R;
+import com.kfcreservation.dao.impl.FoodAllDaoImpl;
+
+public class ActivityCore {
+
+	public SimpleAdapter getFoodImageAdapter(Context context, int FoodTypeId) {
+		String[] f = { "name", "price", "imgs" };
+		int[] t = { R.id.tv_name, R.id.tv_price, R.id.im_img };
+		FoodAllDaoImpl fad = new FoodAllDaoImpl();
+		// List<KFC> kfcs = fb.getbytype(this, idr);
+		List<HashMap<String, Object>> imageList = fad.getFoodAllType(context,
+				FoodTypeId);
+
+		List<HashMap<String, Object>> ls = new ArrayList<HashMap<String, Object>>();
+
+		for (HashMap<String, Object> k : imageList) {
+
+			HashMap<String, Object> mp = null;
+			mp = new HashMap<String, Object>();
+			mp.put("name", k.get("Name"));
+			mp.put("price", k.get("Price"));
+			mp.put("imgs", getBitmap(k.get("Img").toString()));
+			ls.add(mp);
+		}
+
+		SimpleAdapter adapter = new SimpleAdapter(context, ls,
+				R.layout.menu_list, f, t);
+		adapter.setViewBinder(new ViewBinder() {
+
+			@Override
+			public boolean setViewValue(View view, Object data,
+					String textRepresentation) {
+				if (view instanceof ImageView && data instanceof Bitmap) {
+					ImageView iv = (ImageView) view;
+					iv.setImageBitmap((Bitmap) data);
+					return true;
+				}
+				return false;
+			}
+		});
+
+		return adapter;
+	}
+
+	public Bitmap getBitmap(String imageName) {
+		String path = Environment.getExternalStorageDirectory()
+				+ File.separator + "kfcpng" + File.separator + imageName
+				+ ".png";
+		Bitmap bitmap = BitmapFactory.decodeFile(path);
+		return bitmap;
+	}
 }
