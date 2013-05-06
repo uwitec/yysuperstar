1da74fc2c00da5d1c156202857a4a548af74a612
yrenjie289 <yrenjie289@gmail.com>
2013-05-06 05:17:32 +0000

    1.目录结构更新 2.项目合并

---
+++ KF1.0/src/com/kfcreservation/handler/KFCLoginHandler.java
@@ -0,0 +1,61 @@
+package com.kfcreservation.handler;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+
+import com.kfcreservation.control.KFCLogin;
+import com.kfcreservation.control.KFCWaiter;
+import com.kfcreservation.control.KFCWaiterNew;
+
+public class KFCLoginHandler {
+	
+	public static Handler LoginHandler = new Handler() {
+
+		@Override
+		public void handleMessage(Message msg) {
+			KFCLogin kfclogin = (KFCLogin) msg.obj;
+			
+			switch (msg.arg1) {
+			case 0:
+				kfclogin.showToast("手机号不能为空");
+				break;
+			case 1:
+				kfclogin.showToast("密码不能为空");
+				break;
+			case 2:
+				kfclogin.showToast("密码错误");
+				break;
+			case 3:
+				kfclogin.showToast("登录成功");
+				Intent i = new Intent();
+				i.setClass(kfclogin, KFCWaiter.class);
+				kfclogin.startActivity(i);
+				break;
+			case 4:
+				kfclogin.showToast("注册失败");
+				break;
+			}
+		}
+	};
+	
+	public static Handler WeiboHandler = new Handler() {
+
+		@Override
+		public void handleMessage(Message msg) {
+			KFCLogin kfclogin = (KFCLogin) msg.obj;
+			
+			switch (msg.arg1) {
+			case 0:
+				kfclogin.showToast("分享成功");
+				break;
+			case 1:
+				kfclogin.showToast("WeiboException");
+				break;
+			case 2:
+				kfclogin.showToast("IOException");
+				break;
+			}
+		}
+	};
+}
